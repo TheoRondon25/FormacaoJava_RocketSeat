978caa1ffbeae34991a41a50ff97958cbc308adb
Theo Rondon <theoprondon@hotmail.com>
2026-07-23 22:29:37 -0300

    feat: Treinando sobre Abstração em Java

--- Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/FacebookMessenger.java
+++ Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/FacebookMessenger.java
@@ -1,4 +1,10 @@
 package je_pilares_poo;
 
 public class FacebookMessenger extends SistemaMensagemInstantanea {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo FACEBOOK Messenger");
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo FACEBOOK Messenger");
+    }
 }
--- Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/SistemaMensagemInstantanea.java
+++ Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/SistemaMensagemInstantanea.java
@@ -1,21 +1,8 @@
 package je_pilares_poo;
 
-public class SistemaMensagemInstantanea {
-    public void enviarMensagem() {
-        if(validarConectadoInternet()){
-            System.out.println("Enviando mensagem");
-        } else {
-            System.out.println("Computador offline");
-        }
-    }
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem");
-    }
-    private boolean validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-        return true;
-    }
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public abstract class SistemaMensagemInstantanea {
+
+    public abstract void enviarMensagem();
+
+    public abstract void receberMensagem();
 }
