e5fbfcbd531e573c7c4c95298e3a91fce4eaddeb
Theo Rondon <theoprondon@hotmail.com>
2026-07-23 22:19:39 -0300

    feat: Treinando sobre encapsulamento em Java

---
+++ Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/ComputadorPedrinho.java
@@ -0,0 +1,9 @@
+package je_pilares_poo;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNMessenger msnMessenger = new MSNMessenger();
+        msnMessenger.enviarMensagem();
+        msnMessenger.receberMensagem();
+    }
+}
---
+++ Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_pilares_poo/MSNMessenger.java
@@ -0,0 +1,21 @@
+package je_pilares_poo;
+
+public class MSNMessenger {
+    public void enviarMensagem() {
+        if(validarConectadoInternet()){
+            System.out.println("Enviando mensagem");
+        } else {
+            System.out.println("Computador offline");
+        }
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+    private boolean validarConectadoInternet() {
+        System.out.println("Validando se está conectado a internet");
+        return true;
+    }
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
