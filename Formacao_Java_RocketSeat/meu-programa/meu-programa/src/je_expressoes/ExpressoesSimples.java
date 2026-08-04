101f540ac29c135af041a3fa81b5a50a709e93b3
Theo Rondon <theoprondon@hotmail.com>
2026-08-03 22:09:39 -0300

    feat: Treinando sobre expressoes avançadas em java

--- Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_expressoes/ExpressoesSimples.java
+++ Formacao_Java_RocketSeat/meu-programa/meu-programa/src/je_expressoes/ExpressoesSimples.java
@@ -1,12 +1,19 @@
 package je_expressoes;
 
+import java.time.LocalDate;
+
 public class ExpressoesSimples {
     public static void main(String[] args) {
-        String nome = "Theo";
+        String nome = "Theo Poletti Rondon";
         Integer matricula = 123;
         Double salario = 1000.50;
-        String dadosFuncionario = String.format("Mat.: %d - Nome: %s - Sal.: %,.2f", matricula, nome, salario);
+        LocalDate dataAdmissao = LocalDate.now();
         //s, d, f, t
+
+        String dataImpressa = String.format("Data: %td/%<tm/%<tY", dataAdmissao);
+        System.out.println(dataImpressa);
+
+        String dadosFuncionario = String.format("Mat.: %05d Nome: %-15.15s Sal.: %,.2f", matricula, nome, salario);
         System.out.println(dadosFuncionario);
     }
 
