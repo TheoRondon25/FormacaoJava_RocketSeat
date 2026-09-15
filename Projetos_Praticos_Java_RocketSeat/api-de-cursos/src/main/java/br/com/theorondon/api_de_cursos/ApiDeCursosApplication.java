e86c4ed9993297bf17753a3436e1d436eb580e82
Theo Rondon <theoprondon@hotmail.com>
2026-09-14 21:05:45 -0300

    Criando o projeto pratico API de Cursos, solicitado no Nivel 4 da formaçao de Java RocketSeat

---
+++ Projetos_Praticos_Java_RocketSeat/api-de-cursos/src/main/java/br/com/theorondon/api_de_cursos/ApiDeCursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.theorondon.api_de_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeCursosApplication.class, args);
+	}
+
+}
---
+++ Projetos_Praticos_Java_RocketSeat/api-de-cursos/src/main/java/br/com/theorondon/api_de_cursos/modules/CursosEntity.java
@@ -0,0 +1,42 @@
+package br.com.theorondon.api_de_cursos.modules;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data 
+@Entity
+@Table(name = "cursos")
+public class CursosEntity {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;    
+
+    @NotBlank(message = "O campo [name] não pode estar em branco")
+    private String name;    
+    
+    @NotBlank(message = "O campo [category] não pode estar em branco")
+    private String category;
+    
+    @NotBlank(message = "O campo [professor] não pode estar em branco")
+    private String professor;
+    
+    private boolean active = true;
+
+    @CreationTimestamp 
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp 
+    private LocalDateTime updatedAt;
+}
