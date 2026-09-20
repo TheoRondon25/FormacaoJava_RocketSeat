2338ca2ea931f972db0b9e490ce96dde81c50a65
Theo Rondon <theoprondon@hotmail.com>
2026-09-20 20:59:53 -0300

    feat: Criando a rota DELETE

--- Projetos_Praticos_Java_RocketSeat/api-de-cursos/src/main/java/br/com/theorondon/api_de_cursos/modules/controllers/CursosController.java
+++ Projetos_Praticos_Java_RocketSeat/api-de-cursos/src/main/java/br/com/theorondon/api_de_cursos/modules/controllers/CursosController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,16 @@ public class CursosController {
             var cursoSalvo = this.cursosRepository.save(curso);
             return ResponseEntity.ok(cursoSalvo);
     }
-    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+
+        if (!this.cursosRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        this.cursosRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }    
 
 }
