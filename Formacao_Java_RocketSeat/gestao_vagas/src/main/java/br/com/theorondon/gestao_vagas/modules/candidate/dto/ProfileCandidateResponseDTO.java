b910e00e375b6fcf4fcff1f94aba5bfc8d023f42
Theo Rondon <theoprondon@hotmail.com>
2026-09-22 21:26:19 -0300

    refactor: Mais alguns ajustes referentes a documentaçao no swagger e definindo retorno de erro

--- Formacao_Java_RocketSeat/gestao_vagas/src/main/java/br/com/theorondon/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ Formacao_Java_RocketSeat/gestao_vagas/src/main/java/br/com/theorondon/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.theorondon.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor 
 public class ProfileCandidateResponseDTO {
     
+    @Schema(example = "Desenvolvedor Java")    
     private String description;
+
+    @Schema(example = "theo")
     private String username;
+
+    @Schema(example = "theo@email.com")
     private String email;
+    
     private UUID id;
+    
+    @Schema(example = "Theo Rondon")
     private String name;
 }
