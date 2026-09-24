8fd6b4ef1760110b21d0cbf7b97fa99ad8b70d89
Theo Rondon <theoprondon@hotmail.com>
2026-09-23 21:12:18 -0300

    refactor: Refatorando a documentaçao para finalizar o modulo

--- Formacao_Java_RocketSeat/gestao_vagas/src/main/java/br/com/theorondon/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ Formacao_Java_RocketSeat/gestao_vagas/src/main/java/br/com/theorondon/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -32,6 +32,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {    
 
     @Autowired
@@ -44,6 +45,13 @@ public class CandidateController {
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
     
     @PostMapping("/")
+    @Operation(summary = "Cadastro do candidato", description = "Essa função é responsável por cadastrar um candidato")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = CandidateEntity.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "User not found")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -55,8 +63,7 @@ public class CandidateController {
     }
 
     @GetMapping("/")
-    @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
+    @PreAuthorize("hasRole('CANDIDATE')")    
     @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
@@ -77,8 +84,7 @@ public class CandidateController {
     }
 
     @GetMapping("/job")
-    @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
+    @PreAuthorize("hasRole('CANDIDATE')")    
     @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponiveis, baseada no filtro")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
